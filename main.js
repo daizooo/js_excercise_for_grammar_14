702f2864af5282cdebe5d85fff1c30605b7e17dc
overflow326 <s.daizou@outlook.com>
2019-02-28 22:25:37 +0900

    test

--- main.js
+++ main.js
@@ -8,9 +8,25 @@
 //   - weight: 体重
 // - インスタンスごとの固有の情報はコンストラクタ経由でセットする
 
+class Person {
+  constructor(name, age, gender, height, weight) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.height = height;
+    this.weight = weight;
+  }
+}
 
 // 課題2: 自分もしくは、架空の人の情報を使って１人の人間を表すPersonインスタンスを生成する
 // - インスタンスは変数 `me` に代入する
 
+const me = new Person("だいぞう", 25, "オス", 171, 73);
 
 // 課題3: 課題2で生成したインスタンスの情報をconsole.logを使って、「名前」「年齢」「性別」「身長」「体重」が正しくセットされていることを確認する
+
+console.log(me.name);
+console.log(me.age);
+console.log(me.gender);
+console.log(me.height);
+console.log(me.weight);
